7709ebb3228775982f0d5cc844dce7cdddf6e436
Simple <157860608+vishala5000@users.noreply.github.com>
2026-09-22 08:09:36 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,19 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
+
 rootProject.name = "KittenTTS"
 include(":app")
