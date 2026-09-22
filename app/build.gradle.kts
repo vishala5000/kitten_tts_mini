d647907ae7ddc73e55359e7e8d65a028cb2b8250
Simple <157860608+vishala5000@users.noreply.github.com>
2026-09-22 08:35:19 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 android {
     namespace = "com.vishala.kittentts"
+
     compileSdk = 36
 
     defaultConfig {
@@ -15,9 +16,14 @@ android {
 
         versionCode = 1
         versionName = "1.0"
+
+        ndk {
+            abiFilters += "arm64-v8a"
+        }
     }
 
     buildTypes {
+
         release {
             isMinifyEnabled = false
             isShrinkResources = false
@@ -33,18 +39,44 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(
+                org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+            )
+        }
     }
 
     packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    sourceSets {
+        getByName("main") {
+            assets.srcDir(
+                "src/main/assets"
+            )
+
+            jniLibs.srcDir(
+                "src/main/jniLibs"
+            )
         }
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
+
+    implementation(
+        "androidx.core:core-ktx:1.17.0"
+    )
+
+    implementation(
+        "androidx.appcompat:appcompat:1.7.1"
+    )
 }
