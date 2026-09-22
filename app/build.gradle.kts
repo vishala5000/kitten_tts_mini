aa89a3c398cfa567358187aca24b3e2264384c4d
Simple <157860608+vishala5000@users.noreply.github.com>
2026-09-22 08:09:08 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,42 +4,43 @@ plugins {
 }
 
 android {
-    namespace = "com.vishala.kitten"
-    compileSdk = 35
+    namespace = "com.vishala.kittentts"
+    compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.vishala.kitten"
-        minSdk = 26
-        targetSdk = 35
+        applicationId = "com.vishala.kittentts"
+        minSdk = 23
+        targetSdk = 36
+
         versionCode = 1
         versionName = "1.0"
-    }
-
-    ndkVersion = "27.0.12077973"
-
-    packaging {
-        jniLibs.useLegacyPackaging = true
-        resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
             isShrinkResources = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+        }
+
+        debug {
+            isMinifyEnabled = false
         }
     }
-
-    sourceSets["main"].jniLibs.srcDir("src/main/jniLibs")
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
